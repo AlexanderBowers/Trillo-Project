c4dd46ff81c2ebaf9769873c7c52a10151a281c9
AlexanderBowers <7319abowers@gmail.com>
2021-01-25 16:47:41 -0800

    added event listener

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,3 +2,18 @@ const BASEURL = 'http://localhost:3000'
 const LISTURL = `${BASEURL}/lists`
 const TASKURL = `${BASEURL}/tasks`
 const BOARDURL = `${BASEURL}/boards`
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchBoards();
+  });
+  function fetchBoards() {
+    fetch(`${BASEURL}/boards`)
+    .then (res => res.json())
+    .then (board => renderBoard(board))
+  }
+  function renderBoard(board) {
+    let div = document.querySelector("main")
+    let h3 = document.createElement('h3')
+    h3.textContent = board[0].name
+    main.append(h3)
+  }
